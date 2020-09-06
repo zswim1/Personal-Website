5eeb848d7c89395a654ec958cef75ba8e71d21f2
Zachary Swim <zswim@purdue.edu>
2020-09-06 12:03:40 -0500

    Updates

---
+++ precache-manifest.e9d75188e2c3c1010579dccefd8ce5a8.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28918ac6e682cb03102627c761318fca",
+    "url": "/Personal-Website/index.html"
+  },
+  {
+    "revision": "ea63c39239d03235f75c",
+    "url": "/Personal-Website/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "242d0b59a84bd09d2948",
+    "url": "/Personal-Website/static/css/main.64327c0d.chunk.css"
+  },
+  {
+    "revision": "ea63c39239d03235f75c",
+    "url": "/Personal-Website/static/js/2.3d77a1bc.chunk.js"
+  },
+  {
+    "revision": "ebec6ec5880fcec562f9a7747ae47b51",
+    "url": "/Personal-Website/static/js/2.3d77a1bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "242d0b59a84bd09d2948",
+    "url": "/Personal-Website/static/js/main.3469dff4.chunk.js"
+  },
+  {
+    "revision": "6125ad4d5e622a1e7fdb",
+    "url": "/Personal-Website/static/js/runtime-main.a04e95f6.js"
+  },
+  {
+    "revision": "a2bbc2d6cfd1b7b0aaa52ef1d2530ef6",
+    "url": "/Personal-Website/static/media/CourseMaster.a2bbc2d6.png"
+  },
+  {
+    "revision": "f6072aa014e98ae33fa6b362b02e2b5e",
+    "url": "/Personal-Website/static/media/Family.f6072aa0.jpg"
+  },
+  {
+    "revision": "d767a0c9762c38978ef132544a21bc8d",
+    "url": "/Personal-Website/static/media/Grandpa.d767a0c9.jfif"
+  },
+  {
+    "revision": "89f76b168cb81bacce73d17454ef7d67",
+    "url": "/Personal-Website/static/media/Headshot.89f76b16.jpg"
+  },
+  {
+    "revision": "e8e410d5ac123fa63e4080faa4489409",
+    "url": "/Personal-Website/static/media/Milliman.e8e410d5.jpg"
+  },
+  {
+    "revision": "24f924d0b92be2de80539d2b4d8439c8",
+    "url": "/Personal-Website/static/media/Mountain.24f924d0.jfif"
+  },
+  {
+    "revision": "510d4984344805a90db1397a9608c16f",
+    "url": "/Personal-Website/static/media/Travel.510d4984.png"
+  },
+  {
+    "revision": "983be2ba40ff3975b7d6190444cbcb3c",
+    "url": "/Personal-Website/static/media/basketball.983be2ba.png"
+  },
+  {
+    "revision": "349d5ecef0e937ac46cad2b0b82b94af",
+    "url": "/Personal-Website/static/media/bike.349d5ece.png"
+  },
+  {
+    "revision": "43bcba3353479cc56d6a725adc306a23",
+    "url": "/Personal-Website/static/media/drone.43bcba33.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Personal-Website/precache-manifest.e9d75188e2c3c1010579dccefd8ce5a8.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Personal-Website/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
